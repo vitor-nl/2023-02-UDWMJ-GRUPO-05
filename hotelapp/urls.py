41c191ba28a860cf9168627a247c0103d19da451
Filipe Mello <filipem.mostardeiro@gmail.com>
2023-11-27 22:40:34 -0300

    adicionando base do html

--- hotelapp/urls.py
+++ hotelapp/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls', namespace='core')),
     path('categoriaquarto/', include('categoriaquarto.urls', namespace='categoriaquarto')),
     path('quarto/', include('quarto.urls', namespace='quarto')),
     path('servicoadicional/', include('servicoadicional.urls', namespace='servicoadicional')),
